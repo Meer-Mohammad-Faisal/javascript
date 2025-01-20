4177cc1342c7f9b19d44ce2c4f4d4721527fe332
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-01-20 06:54:15 +0000

    dataTypes

--- 1_basic/2_dataTypes.js
+++ 1_basic/2_dataTypes.js
@@ -5,3 +5,18 @@
 console.log(3+3);
 
 console.log("faisal");
+
+//string:
+// 1.number => 2 to 53
+// 2.bigint
+// 3.string => " "
+// 4.boolean => true/false
+// 5.null => standalone value
+// undefined =>
+// symbol => unique.
+
+
+//OBJECT
+
+console.log( typeof null); //object
+console.log( typeof undefined) // undefined
