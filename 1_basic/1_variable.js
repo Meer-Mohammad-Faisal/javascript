4d923ff14760a213f45a20a8efc77b5d977ba18f
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-01-20 05:59:50 +0000

    done

---
+++ 1_basic/1_variable.js
@@ -0,0 +1,15 @@
+const accountId = 646;
+
+let accountName = "faisal";
+
+var accountEmail = "faisal@gmail.com";
+
+accountAddress = "supaul";
+
+//try to change 
+//accountId = 67; can't change.
+accountEmail = "fje@gmail.com";
+accountName = "badal";
+accountAddress = "bhopal";
+
+console.table([accountEmail, accountName, accountAddress, accountId]);
---
+++ 1_basic/2_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict " // treat all code as newer version
+
+//alert(3 + 3) //we are using node not browser
+
+console.log(3+3);
+
+console.log("faisal");
