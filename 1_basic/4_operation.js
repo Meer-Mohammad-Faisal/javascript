45b5d10bce2ef2e1dff8276b8b49eaaace5b68b3
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-01-20 07:56:06 +0000

    operation on dataTypes

---
+++ 1_basic/4_operation.js
@@ -0,0 +1,34 @@
+// assigning negative value
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+//basic operation:
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%3)
+
+// string concatination:
+let str1 = "Meer";
+let str2 = "faisal";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// some extra points 
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+//tricky conversion
+console.log(+true);
+console.log(+"");
+
+//incremental:
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
