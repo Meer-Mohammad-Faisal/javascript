d26c1492481adbf92c34338e54de134cea7a2595
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-01-21 14:59:39 +0000

    maths changes

--- 1_basic/9_numsAndMaths.js
+++ 1_basic/9_numsAndMaths.js
@@ -21,4 +21,18 @@ console.log(hundreds.toLocaleString('en-IN'));
 console.log(Math);
 console.log(Math.abs(-3));
 console.log(Math.round(3.6));
-console.log(Math.ceil(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,9,3,6,7,9));
+console.log(Math.max(4,6,8,3,9));
+//random(imp)
+console.log(Math.random()) // anything between 0 to 1
+console.log(Math.random()*10); // from 0 to 10
+console.log((Math.random()*10) + 1); //1 is making sure there is not 0
+console.log(Math.floor(Math.random()*10) + 1); // give witout point
+
+//basic formula:
+const min = 10; 
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min ) + 1) + min);
