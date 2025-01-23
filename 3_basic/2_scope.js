eb09c1868027b3024951002cf05446a0b62b7ad3
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-01-23 18:15:25 +0000

    scopes

--- 3_basic/2_scope.js
+++ 3_basic/2_scope.js
@@ -16,3 +16,58 @@ console.log(c); // give inner value of c = 300
 
 
 // NOTE: scope is diffrent on browser or my diffrent on codespace(vs code)
+
+
+//NASTED SCOPE:
+function one(){
+    const username = "faisal";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+        
+    }
+    //console.log(website);
+
+    two();
+}
+
+one();
+
+
+
+if (true) {
+    const username = "fasial"
+    if (username === "fasial") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    //console.log(website); // cannot access
+}
+
+//console.log(username); // cannot access
+
+
+
+
+// +++++++++++++++++++ intresting +++++++++++++++
+
+
+console.log(addone(4)); // access before the function
+
+function addone (num) {
+    return num + 1;
+}
+
+
+
+
+// function ko varable me store kr leya
+
+//addTwo(5);  // cannot access before the variable of function
+
+const addTwo = function(num){
+    return num + 2;
+}
+
+console.log(addTwo(5));
